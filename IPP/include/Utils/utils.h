620b0e8a96bf139eb7e4c8e3c89c1e09b67fcb6d
Diego Antognini <diegoantognini@gmail.com>
2014-03-27 17:13:10 +0100

    Add utils function vector

--- IPP/include/Utils/utils.h
+++ IPP/include/Utils/utils.h
@@ -12,4 +12,28 @@ namespace ipp_random
     std::vector<double> randDoubleList(double a, double b, int n);// return a <= x < b, n times
 }
 
+namespace ipp_utils
+{
+    template<typename T>
+    void mergeVectors(std::vector<T>& v1, const std::vector<std::vector<T>>& v2)
+    {
+        for(auto& v:v2)
+            v1.insert(v1.end(), v.begin(), v.end());
+    }
+
+    template<typename T>
+    std::vector<std::vector<T>> createSubSamples(const std::vector<T>& sets, const int k)
+    {
+        std::vector<std::vector<T>> samples;
+        int sizeSample = sets.size()/k;
+        for(int i = 0; i < k-1; ++i)
+        {
+            samples.push_back(std::vector<T>());
+            samples.back().insert(samples.back().end(), sets.begin()+i*sizeSample, sets.begin()+(i+1)*sizeSample);
+        }
+        samples.back().insert(samples.back().end(), sets.begin()+(k-1)*sizeSample, sets.end());
+        return samples;
+    }
+}
+
 #endif // UTILS_H
