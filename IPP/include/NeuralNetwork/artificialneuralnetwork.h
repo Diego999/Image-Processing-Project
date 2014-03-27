a6dfe4475876555ebb738c1ee7ac9386f28dae87
Diego Antognini <diegoantognini@gmail.com>
2014-03-27 20:24:04 +0100

    Add mergeANNs method

--- IPP/include/NeuralNetwork/artificialneuralnetwork.h
+++ IPP/include/NeuralNetwork/artificialneuralnetwork.h
@@ -13,6 +13,8 @@ public:
     //The inputs is not consider as a layer !
     ArtificialNeuralNetwork(int nbInputs, int nbOutputs, const std::vector<int>& nbNeuronsPerHiddenLayer, double learningRate, double momentum);
     ArtificialNeuralNetwork(const ArtificialNeuralNetwork& ann);
+    ArtificialNeuralNetwork(const std::vector<ArtificialNeuralNetwork>& anns);
+    ArtificialNeuralNetwork(const std::vector<std::shared_ptr<ArtificialNeuralNetwork>>& anns);
     ~ArtificialNeuralNetwork();
 
     double learningRate() const {return m_learningRate;}
@@ -42,6 +44,7 @@ public:
     void prevDeltaThreshold(int numLayer, int numNeuron, double d);
 
 private:
+    void mergeANNs(const std::vector<ArtificialNeuralNetwork>& anns);
     void layerForward();
     double computeHiddenError();
     double computeOutputError();
