e2e5eea081f499a0ca4a283ff483e990c28372d1
Diego Antognini <diegoantognini@gmail.com>
2014-04-03 14:57:59 +0200

    Fix bug Kcrossvalidation

--- IPP/include/Tests/tests.h
+++ IPP/include/Tests/tests.h
@@ -3,8 +3,9 @@
 
 void testsANN();
 void testsANNController();
-void testsANNMerge();
 
 void testMergeVector();
 void testCreateSubSamples();
+void testKFoldCrossValidation();
+
 #endif // TESTS_IPP_H
